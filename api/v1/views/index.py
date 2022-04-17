7f26ac1b7a364f5cb423bab94d2f8110f6d2d91b
avocadoclouds <rehana447@gmail.com>
2022-04-17 22:46:03 +0300

    task 4: update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,30 +4,32 @@ flask route that returns a status response (json)
 """
 
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, request
 from models import storage
 
 
 
-@app_views.route('/status')
+@app_views.route('/status', methods=['GET'])
 def status():
     """
     function for status route that returns the status.
     """
-    response = {"status": "OK"}
-    return jsonify(response)
+    if request.method == 'GET':
+        response = {"status": "OK"}
+        return jsonify(response)
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', methods=['GET'])
 def stat():
     """ function  to return count of objects """
-    result = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User),
-    }
+    if request.method == 'GET':
+        result = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User),
+        }
 
-    return jsonify(result)
+        return jsonify(result)
