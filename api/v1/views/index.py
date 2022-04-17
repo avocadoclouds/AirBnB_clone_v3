e1dad7d7fd8e844cd00170a964accd0117252740
avocadoclouds <rehana447@gmail.com>
2022-04-17 22:29:27 +0300

    task 4: count() method from storage

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""import app_views from api.v1.views
-create a route /status on the object app_views that
-returns a JSON: "status": "OK"
+"""
+flask route that returns a status response (json)
 """
 
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
 
 
 @app_views.route('/status')
@@ -15,3 +16,18 @@ def status():
     """
     response = {"status": "OK"}
     return jsonify(response)
+
+
+@app_views.route('/stats')
+def stat():
+    """ function  to return count of objects """
+    result = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
+    }
+
+    return jsonify(result)
