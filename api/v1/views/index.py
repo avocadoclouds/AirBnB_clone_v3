54f44826680850133f49048d7138cf1cfbffd61d
avocadoclouds <rehana447@gmail.com>
2022-04-17 22:50:48 +0300

    fix pep8

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 """
-flask route that returns a status response (json)
+flask route that returns a status response (json).
 """
 
 from api.v1.views import app_views
 from flask import jsonify, request
 from models import storage
-
 
 
 @app_views.route('/status', methods=['GET'])
