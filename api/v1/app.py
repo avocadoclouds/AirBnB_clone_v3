fd468be73622fd07789a11d10aeeec5b28c294ef
avocadoclouds <rehana447@gmail.com>
2022-04-18 00:12:39 +0300

    task 5: 404 status

--- api/v1/app.py
+++ api/v1/app.py
@@ -28,9 +28,7 @@ def teardown_db(exception):
 @app.errorhandler(404)
  def page_not_found(error):
      """Route to handle 404 status"""
-     response = {"error": "Not found"}
-     resp = make_response(jsonify(response), 404)
-     return resp
+     make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
