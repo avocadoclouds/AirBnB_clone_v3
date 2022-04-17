e25e01bb7b76e1b9e9ac188f57e889efb9901b83
avocadoclouds <rehana447@gmail.com>
2022-04-17 07:43:38 +0300

    fix identation

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,9 @@ from flask import jsonify
 from api.v1.views import app_views
 
 @app_views.route('/status')
- def status():
-     """
-     function for status route that returns the status
-     """
-     resp = {"status": "OK"}
-     return jsonify(resp)
+def status():
+    """
+    function for status route that returns the status
+    """
+    response = {"status": "OK"}
+    return jsonify(response)
