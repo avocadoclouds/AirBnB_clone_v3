242b08f3069b3931fd89943b620ea5afa63daa41
avocadoclouds <rehana447@gmail.com>
2022-04-17 22:07:11 +0300

    shebang fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+"""import app_views from api.v1.views
+create a route /status on the object app_views that
+returns a JSON: "status": "OK"
+"""
+
 from api.v1.views import app_views
 from flask import jsonify
 
