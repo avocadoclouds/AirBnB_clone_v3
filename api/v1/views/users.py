245b541a1acce8a9db6a7a37f5b86b72efb758c3
avocadoclouds <rehana447@gmail.com>
2022-04-18 21:32:30 +0300

    task 9: update users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -4,6 +4,9 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from models import storage
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
 from models.user import User
 
 
