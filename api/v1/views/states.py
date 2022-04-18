0d151ac3daab9b81df70e9d4211814c3a2af79c9
avocadoclouds <rehana447@gmail.com>
2022-04-18 06:27:02 +0300

    task 6: states

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,6 +4,7 @@ that handles all default RESTFul API"""
 from api.v1.views import app_views
 from flask import jsonify, make_response, request
 from models import storage
+from models.state import State
 
 
 @app_views.route('/states', methods=['GET'])
