018fcb617d4a8a7e477624e540cbff4b4d4b3387
avocadoclouds <rehana447@gmail.com>
2022-04-19 00:04:10 +0300

    update init file for task 11

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+"""Initialize Blueprint views"""
+
 from flask import Blueprint
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 from api.v1.views.index import *
@@ -6,4 +9,4 @@ from api.v1.views.cities import *
 from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-from api.v1.views.place_reviews import *
+from api.v1.views.places_reviews import *
