c3daed8e3a8ed7fe9efdd324617dc94b88400217
avocadoclouds <rehana447@gmail.com>
2022-04-18 07:40:27 +0300

    task 3: fix;

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """Flask App"""
 
 from api.v1.views import app_views
-from flask import Flask, render_template, jsonify, url_for
+from flask import Flask, render_template, jsonify
 from models import storage
 import os
 
@@ -20,16 +20,9 @@ port = os.getenv('HBNB_API_PORT', 5000)
 @app.teardown_appcontext
 def teardown_db(exception):
     """calls storage.close() that
-    on current sqlalchemy sessions.
+    on current sqlalchemy sessions
     """
     storage.close()
-
-
-@app.errorhandler(404)
- def page_not_found(error):
-     """Route to handle 404 status"""
-     response = jsonify({"error": "Not found"}), 404
-     return response
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
-"""
-flask route that returns a status response (json).
+"""import app_views from api.v1.views
+create a route /status on the object app_views that
+returns a JSON: "status": "OK"
 """
 
 from api.v1.views import app_views
-from flask import jsonify, request
-from models import storage
+from flask import jsonify
 
 
-@app_views.route('/status', methods=['GET'])
+@app_views.route('/status')
 def status():
     """
     function for status route that returns the status.
     """
-    if request.method == 'GET':
-        response = {"status": "OK"}
-        return jsonify(response)
-
-
-@app_views.route('/stats', methods=['GET'])
-def stats():
-    """ function  to return count of objects """
-    if request.method == 'GET':
-        response = {}
-        result = {
-            "Amenity": "amenities",
-            "City": "cities",
-            "Place": "places",
-            "Review": "reviews",
-            "State": "states",
-            "User": "users"
-        }
-         for key, value in result.items():
-             response[value] = storage.count(key)
-        return jsonify(response)
+    response = {"status": "OK"}
+    return jsonify(response)
