6ef20a7fecc6f4cfecf70b87d83648059d3b5c43
Beldine-Moturi <beldinemoturi@gmail.com>
2022-04-18 16:10:34 +0300

    task 10: import Place and City

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -6,6 +6,8 @@ that handles all default RESTFul API actions:
 from api.v1.views import app_views
 from flask import jsonify, make_response, request, abort
 from models import storage
+from models import Place
+from models import City
 
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'])
