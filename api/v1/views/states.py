62cd67f27aa0f1e14b7e4db76907e2a8590e5897
avocadoclouds <rehana447@gmail.com>
2022-04-18 06:54:36 +0300

    task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,7 +2,7 @@
 """Create a new view for State objects
 that handles all default RESTFul API"""
 from api.v1.views import app_views
-from flask import jsonify, make_response, request
+from flask import jsonify, make_response, abort, request
 from models import storage
 from models.state import State
 
